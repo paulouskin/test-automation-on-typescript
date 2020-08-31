be51860219eef746d21c27d2b95ef7697eb35d9a
Ivan Paulouski <pavlovskin88@gmail.com>
2020-08-31 11:40:29 +0200

    Added Stryker usage example

--- test/queue.spec.ts
+++ test/queue.spec.ts
@@ -7,9 +7,14 @@ describe('Queue', () => {
         console.log('Here can be your test setup activities');
     });
 
-    it('should be empty after initialization without parameters', () => {
+    it('should have size equal to 0 after initialization without parameters', () => {
         const queue = new Queue<number> ();
         expect(queue.size()).to.equal(0);
+    }) 
+    
+    it('should be empty after initialization with empty array', () => {
+        const queue = new Queue<number> ([]);
+        expect(queue.isEmpty()).to.equal(true);
     }) 
 
     it('should be initialized with constructor arguments', () => {
@@ -23,6 +28,7 @@ describe('Queue', () => {
         queue.push(2);
         queue.push(3);
         expect(queue.size()).to.equal(2);
+        expect(queue.isEmpty()).to.equal(false);
     })
 
     it('should be able to pop element from the top', () => {
