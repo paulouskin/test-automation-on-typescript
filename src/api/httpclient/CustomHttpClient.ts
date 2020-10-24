6c4451dd693887d2b9ed5cebf0845a1520fe5cf9
Ivan Paulouski <pavlovskin88@gmail.com>
2020-10-24 22:33:40 +0200

    Added mochawesome test reporter and cookies setup

--- src/api/httpclient/CustomHttpClient.ts
+++ src/api/httpclient/CustomHttpClient.ts
@@ -1,6 +1,6 @@
 export interface CustomHttpClient {
-    getRequest(host: string, endpoint: string, headers: Map<string, string>, query?: string): Promise<any>;
-    postRequest(host: string, endpoint: string, headers: Map<string, string>, body: any): Promise<any>;
-    putRequest(host: string, endpoint: string, headers: Map<string, string>, body: any): Promise<any>;
-    deleteRequest(host: string, endpoint: string, headers: Map<string, string>): Promise<any>;
+    getRequest(host: string, endpoint: string, headers: Map<string, string>, query?: string, cookies?: Map<string, string>): Promise<any>;
+    postRequest(host: string, endpoint: string, headers: Map<string, string>, body: any, cookies?: Map<string, string>): Promise<any>;
+    putRequest(host: string, endpoint: string, headers: Map<string, string>, body: any, cookies?: Map<string, string>): Promise<any>;
+    deleteRequest(host: string, endpoint: string, headers: Map<string, string>, cookies?: Map<string, string>): Promise<any>;
 }
--- src/api/httpclient/SupertestClient.ts
+++ src/api/httpclient/SupertestClient.ts
@@ -2,20 +2,21 @@ import supertest from "supertest";
 import { CustomHttpClient } from "./CustomHttpClient";
 
 export class SupertestClient implements CustomHttpClient {
+    
+    postRequest(host: string, endpoint: string, headers: Map<string, string>, body: any, cookies?: Map<string, string>): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+    putRequest(host: string, endpoint: string, headers: Map<string, string>, body: any, cookies?: Map<string, string>): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
+    deleteRequest(host: string, endpoint: string, headers: Map<string, string>, cookies?: Map<string, string>): Promise<any> {
+        throw new Error("Method not implemented.");
+    }
 
-    postRequest(host: string, endpoint: string, headers: Map<string, string>, body: any): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    putRequest(host: string, endpoint: string, headers: Map<string, string>, body: any): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-    deleteRequest(host: string, endpoint: string, headers: Map<string, string>): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-
-    getRequest(host: string, endpoint: string, headers: Map<string, string>, query = ''): Promise<any> {
+    getRequest(host: string, endpoint: string, headers: Map<string, string>, query = '', cookies?: Map<string, string>): Promise<any> {
         let req = supertest(host).get(endpoint + query);
         this.setHeaders(req, headers);
+        if (cookies) { this.setCookies(req, cookies) };
         return req;
     }
 
@@ -24,6 +25,12 @@ export class SupertestClient implements CustomHttpClient {
             req.set(header, headers.get(header)!)
         }
         return req;
+    }
+
+    private setCookies(req: supertest.Test, cookies: Map<string, string>): supertest.Test {
+        const cookiesArray = new Array<string>();
+        cookies.forEach((k,v) => cookiesArray.push(`${k}=${v}`));
+        return req.set('Cookie', cookiesArray);
     }
    
 }
